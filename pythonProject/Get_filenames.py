0e3a209e059672cad804788e337237f5f925a177
Liang Xu <xl0418@gmail.com>
2024-05-21 13:19:20 -0700

    updated

--- pythonProject/Get_filenames.py
+++ pythonProject/Get_filenames.py
@@ -19,3 +19,23 @@ all_fnas = get_filenames(path)
 df = pd.DataFrame(all_fnas, columns=['filename'])
 df.to_csv('all_filenames.csv', index=False)
 
+
+
+#### add kingdoms ####
+#read xlsx
+kingdoms = pd.read_excel('MAGsKingdom.xlsx')
+genomes = pd.read_csv('all_genomes.txt', header=None)
+# add kingdom to genomes
+genomes['kingdom'] = ''
+
+for i in range(len(genomes)):
+    genome_name = genomes.iloc[i,0].split('/')[-1]
+    try:
+        kingdom = kingdoms[kingdoms['Name'] == genome_name]['kingdom'].values[0]
+        genomes['kingdom'][i] = kingdom
+    except:
+        print(genome_name)
+        genomes['kingdom'][i] = 'NA'
+
+# save to csv
+genomes.to_csv('all_genomes_kingdom.csv', index=False)
--- pythonProject/prokkagenomes.sh
+++ pythonProject/prokkagenomes.sh
@@ -2,8 +2,12 @@
 
 # prokka genomes in parallel for all genomes in the input file
 prokka_data(){
+    IFS=',' read -ra ADDR1 <<< "$1"
+    kingdom=${ADDR1[1]}
+    ADDR2=${ADDR1[2]}
+
     # take the input file and split it by "/"
-    IFS='/' read -ra ADDR <<< "$1"
+    IFS='/' read -ra ADDR <<< "$ADDR2"
     dir1=${ADDR[6]}
     genome1=${ADDR[7]}
 #    echo $dir1
@@ -11,7 +15,13 @@ prokka_data(){
     prokkaoutput="prokkaOutput"
 
     if [[ ! -e $prokkaoutput/$dir1/$genome1 ]]; then
-        singularity exec prokka.sif prokka --norrna --notrna --centre X --compliant --kingdom Bacteria $1 --prefix $genome1 --outdir $prokkaoutput/$dir1/$genome1 #--prefix $prokkaoutput/$dir1
+      if [[ $kingdom == *"Bacteria"* ]]; then
+        singularity exec --bind $1 prokka.sif prokka --norrna --notrna --centre X --compliant --kingdom Bacteria $1 --prefix $genome1 --outdir $prokkaoutput/$dir1/$genome1 #--prefix $prokkaoutput/$dir1
+      elif [[ $kingdom == *"Archaea"* ]]; then
+        singularity exec --bind $1 prokka.sif prokka --norrna --notrna --centre X --compliant --kingdom Archaea $1 --prefix $genome1 --outdir $prokkaoutput/$dir1/$genome1 #--prefix $prokkaoutput/$dir1
+      else
+        singularity exec --bind $1 prokka.sif prokka --norrna --notrna --centre X --compliant --kingdom Bacteria $1 --prefix $genome1 --outdir $prokkaoutput/$dir1/$genome1 #--prefix $prokkaoutput/$dir1
+      fi
     fi
 }
 export -f prokka_data
